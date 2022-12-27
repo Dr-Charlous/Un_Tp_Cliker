6866253f4a57584db5ca67fa659163e8781fa8a2
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2022-12-27 22:00:10 +0100

    Corr

--- Assets/Script/Maingame.cs
+++ Assets/Script/Maingame.cs
@@ -1,7 +1,9 @@
-using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System;
 
 public class Maingame : MonoBehaviour
 {
--- Assets/Script/UpgradeUI.cs
+++ Assets/Script/UpgradeUI.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using System;
 
 public class UpgradeUI : MonoBehaviour
 {
-	public class UpgradeUI : MonoBehaviour
+	public class Upgrade_UI : MonoBehaviour
 	{
 		public Image Image;
 		public Text Text;
